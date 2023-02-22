054c6d0fd5b53dabaf13d38463b36d5c3dd7fa97
Jonathan Kong <jonathan.kong@zendesk.com>
2023-02-22 12:52:25 +0800

    feat: add extension popup functionality

---
+++ popup.js
@@ -0,0 +1,15 @@
+let scrapeEmails = document.getElementById("scrapeEmails")
+
+scrapeEmails.addEventListener("click", async () => {
+  // Get current active tab of window
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+  // Execute script to parse emails on page
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: scrapeEmailsFromPage
+  })
+})
+
+// Function to scrape emails
+function scrapeEmailsFromPage() {}
